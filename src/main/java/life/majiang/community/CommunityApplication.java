576a56659430d11521710fc1e91655ae728dd3aa
luludachiever <luludachiever@gmail.com>
2019-09-02 23:47:55 -0700

    init repo

--- src/main/java/life/majiang/community/CommunityApplication.java
+++ src/main/java/life/majiang/community/CommunityApplication.java
@@ -1,13 +1,10 @@
 package life.majiang.community;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan("life.majiang.community.mapper")
-@EnableScheduling
 public class CommunityApplication {
 
     public static void main(String[] args) {
